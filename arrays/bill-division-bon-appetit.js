0aff0d5648ee089d4cd89c339cd996b33c38f5fc
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-11-17 09:59:44 -0800

    added simple bonappetit function, handles up to 100,000 entries with ease

---
+++ arrays/bill-division-bon-appetit.js
@@ -0,0 +1,34 @@
+//  https://www.hackerrank.com/challenges/bon-appetit/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+// Two friends Anna and Brian, are deciding how to split the bill at a dinner. Each will only pay for the items they consume. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+
+//  bill is the bill array of prices (whole dollar)
+//  k is the index of the item anna doesn't want.
+//  b is the amount anna was charged for the meal.
+
+function bonAppetit(bill, k, b) {
+    // declaring what they didn't share so it's read only once during the reduce statement
+  let annasUneaten = bill[k];
+
+  //   find the fair split bill
+  let fairTotal =
+    bill.reduce((accumTot, currentItem) => accumTot + currentItem
+    );
+    let fairSplit = (fairTotal - annasUneaten) / 2
+
+    // console.log(b);
+    // console.log(fairSplit);
+  if (b-fairSplit === 0) {
+      return console.log("Bon Appetit");
+  } else {
+      return console.log(b-fairSplit);
+  }
+}
+// TODO: what's wrong with the above? it won't count ANY of the entries that match the saved value. BANG
+
+let billA = [1, 2, 3, 4, 10];
+let kA = 4;
+let bA = 8;
+bonAppetit(billA, kA, bA);
+
+// fails at 100k items.
---
+++ arrays/newyearschaos.js
@@ -0,0 +1,43 @@
+// It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of people queued up, and each person wears a sticker indicating their initial position in the queue. Initial positions increment by 1 from 1 at the front of the line to  at the back.
+
+// Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if n=8 and person 5 bribes person 4, the queue will look like this: 1,2,3,5,4,6,7,8.
+
+// Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state!
+
+// Complete the function minimumBribes in the editor below. It must print an integer representing the minimum number of bribes necessary, or Too chaotic if the line configuration is not possible.
+
+
+// things to note:if any entry is more or less than 2 positions from start, OR has had to move more than twice to get to our final, then NO!
+// final state is the array given. initial state would be the ordering operation.
+// 1. so doing a handmade sort method on the array given and keeping track of #moves of each item. 
+// 2. making sure along the way... if the # of moves of any individual got to 3 it instantly returned too chaotic
+// 3.  then totalling #moves
+
+// for 60% of the marbles, n is less than 10^3 (number of items)
+//  for 100% of the marbles, n is less than 10^5
+
+// OR: COULD walk the array. take each individual item and see if it is less than 3 spaces from where it started... 
+
+// but what about solutions that swap backward, and not in a straight line, to work? would have to keep track of every single move, maybe in a hashmap?
+
+//  a bribe involves the briber moving left, and the bribee moving right.
+
+// arrays: 
+let a = [2,1,5,3,4] //expect 3
+let b = [2,5,1,4,3] //expect "Too Chaotic"
+
+function minimumBribes(q) {
+    // what do we care about?
+    
+    for (let person of q) {
+        // walk backward through the list
+        // check if a person is "near their spot" or within 2 of i+1 
+        // if not, escape! return Too Chaotic,
+        //if so....
+
+    }
+    // return EITHER: min# bribes, or "Too Chaotic" if you can't get there
+}
+
+minimumBribes(a);
+minimumBribes(b);
